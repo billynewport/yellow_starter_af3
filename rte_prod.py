902d9b105aacc54b041264fa8d7e5b1b9e8acfd4
Billy Newport <billy@billynewport.com>
2025-12-19 16:20:17 -0700

    Update datasurface Docker image version to v0.5.13

--- rte_prod.py
+++ rte_prod.py
@@ -66,7 +66,7 @@ def createPSP() -> YellowPlatformServiceProvider:
         yp_assembly=yp_assembly,
         merge_datacontainer=k8s_merge_datacontainer,
         pv_storage_class="longhorn",
-        datasurfaceDockerImage="datasurface/datasurface:v0.5.12",
+        datasurfaceDockerImage="datasurface/datasurface:v0.5.13",
         dataPlatforms=[
             YellowDataPlatform(
                 name="SCD1",
