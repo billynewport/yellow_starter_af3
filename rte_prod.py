bfa576f9f24570ace643f39816b5d7a57f1b0c8b
Billy Newport <billy@billynewport.com>
2026-01-01 07:43:19 -0700

    Remove OTLP endpoint and protocol from createPSP function in rte_prod.py

--- rte_prod.py
+++ rte_prod.py
@@ -66,8 +66,6 @@ def createPSP() -> YellowPlatformServiceProvider:
         merge_datacontainer=k8s_merge_datacontainer,
         pv_storage_class="longhorn",
         datasurfaceDockerImage="datasurface/datasurface:v0.7.0",
-        otlpEndpoint="http://datadog-agent.default.svc.cluster.local:4318/",
-        otlpProtocol="http/protobuf",
         dataPlatforms=[
             YellowDataPlatform(
                 name="SCD1",
